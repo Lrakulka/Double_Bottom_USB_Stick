e116cf3dfb530beeed6bf538a3d88fe1e978c742
Oleksandr_Borysov <Oleksandr_Borysov@epam.com>
2017-06-25 11:17:51 +0300

    Init commit
    The old project on ChibiOS was replaced by current project on FreeRTOS because
     of advantages of FreeRTOS

---
+++ MDK-ARM/RTE/_Double_Bottom_USB_Stick/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Double_Bottom_USB_Stick' 
+ * Target:  'Double_Bottom_USB_Stick' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
